b84d89c04f995d8ea7c813fb476eba4896e7135f
Matt Cooper <matthew_cooper@brown.edu>
2018-10-06 16:44:01 -0400

    wit mask working

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,7 @@ from wit import Wit
 
 client = Wit('DKZUYDUTBR3Z5XN6XR4VB2ORKLH7TJDT')
 
-with open('test-audio/close-mask.m4a') as f:
-  res = client.speech(f, headers={'Content-Type': 'audio/m4a'})
+with open('test-audio/close-mask-verbose.mp3', 'rb') as f:
+  res = client.speech(f, headers={'Content-Type': 'audio/mpeg3'})
   print(res)
+
