d9f41368f080673d3f851208061e80b1cd31a58c
Matt Cooper <matthew_cooper@brown.edu>
2018-10-11 16:00:04 -0400

    added arduinoControl

---
+++ arduinoControl.js
@@ -0,0 +1,7 @@
+openMask = () => {
+    // TODO (sol): Send open mask arduino commands
+}
+
+closeMask = () => {
+    // TODO (sol): Send close mask arduino commands
+}
--- voiceControl.js
+++ voiceControl.js
@@ -1,15 +1,15 @@
-sendCommand = () => {
+sendVoiceCommand = () => {
     let audioFile = $('#audioFile')[0].files[0];
     let formData = new FormData();
     formData.append('speech', audioFile);
 
     let request = new XMLHttpRequest();
-    request.addEventListener('load', handleCommand);
+    request.addEventListener('load', handleVoiceCommand);
     request.open('POST', 'http://127.0.0.1:5000/voice', true);
     request.send(formData);
 }
 
-handleCommand = res => {
+handleVoiceCommand = res => {
     switch(res) {
         case 'open_mask':
             openMask();
