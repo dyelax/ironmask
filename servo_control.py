347b93d01b8c511c99563ad04e2c2b7f49c35424
Matt Cooper <matthew_cooper@brown.edu>
2018-10-17 10:05:53 -0400

    refactor

---
+++ servo_control.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+
+SERVO_PIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+p = GPIO.PWM(SERVO_PIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+
+# duty cycle for 0 degree = (1/20)*100 = 5%
+# duty cycle for 90 degree = (1.5/20)*100 = 7.5%
+# duty cycle for 180 degree = (2/20)*100 = 10%
+
+def servo_test():
+  try:
+    while True:
+      for x in range(5, 10, 0.5):
+        p.ChangeDutyCycle(x)
+        time.sleep(0.03)
+
+      for x in range(10, 5, -0.5):
+        p.ChangeDutyCycle(x)
+        time.sleep(0.03)
+  except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
+def mask_off():
+  try:
+    for x in range(5, 10, 0.5):
+      p.ChangeDutyCycle(x)
+      time.sleep(0.03)
+  except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
+def mask_on():
+  try:
+    for x in range(10, 5, -0.5):
+      p.ChangeDutyCycle(x)
+      time.sleep(0.03)
+  except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
---
+++ wit_control.py
@@ -0,0 +1,38 @@
+from wit_control import Wit
+from pydub import AudioSegment
+import os
+
+WIT_PUB_KEY = 'DKZUYDUTBR3Z5XN6XR4VB2ORKLH7TJDT'
+wit_client = Wit(WIT_PUB_KEY)
+
+
+def to_mp3(in_path, out_path):
+  AudioSegment.from_file(in_path).export(out_path, format="mp3")
+
+
+def wit_mp3(audio_file):
+  return wit_client.speech(audio_file, headers={'Content-Type': 'audio/mpeg3'})
+
+
+def get_wit_response(audio_path='test-audio/close-mask-verbose.mp3'):
+  with open(audio_path, 'rb') as f:
+    orig_ext = os.path.splitext(audio_path)[1]
+
+    if orig_ext == '.mp3':
+      res = wit_mp3(f)
+    else:
+      orig_path = '/tmp/ironmask' + orig_ext
+      if os.path.exists(orig_path):
+        os.remove(orig_path)
+
+      mp3_path = '/tmp/ironmask.mp3'
+      if os.path.exists(mp3_path):
+        os.remove(mp3_path)
+
+      f.save(orig_path)
+      to_mp3(orig_path, mp3_path)
+
+      with open(mp3_path, 'rb') as f:
+        res = wit_mp3(f)
+
+    return res
