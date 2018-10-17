04323e6fa012823e69b202ec90599e212b4f1007
Sol Kim <solkim813@gmail.com>
2018-10-17 10:59:13 -0400

    button is pressed

--- button_control.py
+++ button_control.py
@@ -2,14 +2,17 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False) # Ignore warning for now
 
-BUTTON_PIN = 22
+BUTTON_PIN = 18
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initial value  = off
 
-try:
-	while True:
-		if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-			print("BUTTON PUUUUUUUSHHHHH!!!!!!!!!!")
-except KeyboardInterrupt:
-	p.stop()
-	GPIO.cleanup()
+
+def button_callback(channel):
+	print("BUTTON PUUUUUUUSHHHHH!!!!!!!!!!")
+	
+GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=button_callback)
+
+def button_is_pressed():
+	return (GPIO.input(BUTTON_PIN) == GPIO.HIGH)
+
+GPIO.cleanup() # Clean up
