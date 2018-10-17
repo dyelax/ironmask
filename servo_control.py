ce66b20e69dd6fde39108f01601466390a6785f7
Sol Kim <solkim813@gmail.com>
2018-10-17 10:07:14 -0400

    servo fixes

--- servo_control.py
+++ servo_control.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-SERVO_PIN = 17
-GPIO.setmode(GPIO.BCM)
+SERVO_PIN = 22
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(SERVO_PIN, GPIO.OUT)
 
 p = GPIO.PWM(SERVO_PIN, 50) # GPIO 17 for PWM with 50Hz
@@ -13,33 +13,33 @@ p.start(2.5) # Initialization
 # duty cycle for 180 degree = (2/20)*100 = 10%
 
 def servo_test():
-  try:
-    while True:
-      for x in range(5, 10, 0.5):
-        p.ChangeDutyCycle(x)
-        time.sleep(0.03)
+	try:
+		while True:
+			for x in range(10, 20, 1):
+				p.ChangeDutyCycle(x/2.0)
+				time.sleep(0.03)
 
-      for x in range(10, 5, -0.5):
-        p.ChangeDutyCycle(x)
-        time.sleep(0.03)
-  except KeyboardInterrupt:
-    p.stop()
-    GPIO.cleanup()
+			for x in range(20, 10, -1):
+				p.ChangeDutyCycle(x/2.0)
+				time.sleep(0.03)
+	except KeyboardInterrupt:
+		p.stop()
+		GPIO.cleanup()
 
 def mask_off():
-  try:
-    for x in range(5, 10, 0.5):
-      p.ChangeDutyCycle(x)
-      time.sleep(0.03)
-  except KeyboardInterrupt:
-    p.stop()
-    GPIO.cleanup()
+	try:
+		for x in range(5, 10, 0.5):
+			p.ChangeDutyCycle(x)
+			time.sleep(0.03)
+	except KeyboardInterrupt:
+		p.stop()
+		GPIO.cleanup()
 
 def mask_on():
-  try:
-    for x in range(10, 5, -0.5):
-      p.ChangeDutyCycle(x)
-      time.sleep(0.03)
-  except KeyboardInterrupt:
-    p.stop()
-    GPIO.cleanup()
+	try:
+		for x in range(10, 5, -0.5):
+			p.ChangeDutyCycle(x)
+			time.sleep(0.03)
+	except KeyboardInterrupt:
+		p.stop()
+		GPIO.cleanup()
