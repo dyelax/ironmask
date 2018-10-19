848069ea03a8e67314412b4e6dccfcc0e73d51ff
Matt Cooper <matthew_cooper@brown.edu>
2018-10-19 14:18:32 -0400

    action test

--- jarvis.py
+++ jarvis.py
@@ -1,20 +1,21 @@
 from audio_control import record
 from wit_control import get_wit_response
 # from button_control import button_is_pressed, cleanup
-# from servo_control import mask_on, mask_off
-#
-# def act_on_wit_response(res):
-#   entities = res['entities']
-#
-#   # determine if mask on/off and act
-#   # TODO: play with confidence levels
-#   if any(x for x in entities['intent'] if (x['value'] == 'mask' and x['confidence'] > 0.9)):
-#     if any(x for x in entities['on_off'] if (x['value'] == 'on' and x['confidence'] > 0.5)):
-#       print('on')
-#       mask_on()
-#     elif any(x for x in entities['on_off'] if (x['value'] == 'off' and x['confidence'] > 0.5)):
-#       print('off')
-#       mask_off()
+from servo_control import mask_on, mask_off
+
+
+def act_on_wit_response(res):
+  entities = res['entities']
+
+  # determine if mask on/off and act
+  # TODO: play with confidence levels
+  if any(x for x in entities['intent'] if (x['value'] == 'mask' and x['confidence'] > 0.9)):
+    if any(x for x in entities['on_off'] if (x['value'] == 'on' and x['confidence'] > 0.5)):
+      print('on')
+      mask_on()
+    elif any(x for x in entities['on_off'] if (x['value'] == 'off' and x['confidence'] > 0.5)):
+      print('off')
+      mask_off()
 
 
 if __name__ == '__main__':
@@ -36,4 +37,4 @@ if __name__ == '__main__':
   audio_path = record()
   res = get_wit_response(audio_path)
   print(res)
-  # act_on_wit_response(res)
+  act_on_wit_response(res)
