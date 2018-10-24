7854065bf9936c5c9834ced9f0fa1c50e89498ef
Matt Cooper <matthew_cooper@brown.edu>
2018-10-23 22:11:15 -0400

    test servo

--- planb.py
+++ planb.py
@@ -1,26 +1,27 @@
 from button_control import button_is_pressed, cleanup
-from servo_control import open_mask, close_mask
+from servo_control import open_mask, close_mask, servo_test
 
 mask_open = False
 toggle_ready = True
 
 if __name__ == '__main__':
-  try:
-    while True:
-      if button_is_pressed():
-        if toggle_ready:
-          if mask_open:
-            print("close mask")
-            close_mask()
-            mask_open = False
-          else:
-            print("open mask")
-            open_mask()
-            mask_open = True
-
-        toggle_ready = False
-      else:
-        toggle_ready = True
-
-  except KeyboardInterrupt:
-    cleanup()
+  servo_test()
+  # try:
+  #   while True:
+  #     if button_is_pressed():
+  #       if toggle_ready:
+  #         if mask_open:
+  #           print("close mask")
+  #           close_mask()
+  #           mask_open = False
+  #         else:
+  #           print("open mask")
+  #           open_mask()
+  #           mask_open = True
+  #
+  #       toggle_ready = False
+  #     else:
+  #       toggle_ready = True
+  #
+  # except KeyboardInterrupt:
+  #   cleanup()
